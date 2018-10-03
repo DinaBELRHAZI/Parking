b3ce3f04da35993f63457d40715805a41b8d5959
walkiryon <g.rossinot@gmaail.com>
2018-10-03 11:03:16 +0200

    Ajout de la base du site

---
+++ site/content/accueil.php
@@ -0,0 +1,1 @@
+<h2> contenu du site </h2>
---
+++ site/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Site parking</title>
+ <!--Header-->
+  <h1> Header du site</h1>
+</head>
+  <body>
+   <!--ne pas modifier cette partie-->
+  <?= $content; ?>
+  
+  			<!-- footer -->
+	<h3> footer </h3>
+  </body>
+</html>
