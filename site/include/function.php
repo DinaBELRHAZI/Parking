3452012449bf73d5e6509e0657204ed4140e2cf8
Guillaume <g.rossinot@gmail.com>
2018-10-19 10:43:19 +0200

    modification et correction du fichier function

--- site/include/function.php
+++ site/include/function.php
@@ -1,12 +1,9 @@
 <?php
-require "config.php"
+require "config.php";
+
 try
 {
-<<<<<<< HEAD
-$bdd = new PDO(mysql:host=$host;dbname=$dbname, $user, $password);
-=======
-$bdd = new PDO('mysql:host=localhost;dbname=parking', 'root', 'root');
->>>>>>> a81e9c5234ade33c061424dbb33749931bbc07a5
+$bdd = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
 }
 catch (Exception $e)
 {
